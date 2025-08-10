faa552d54a5fd2da2a6c56dc467b1ab14d315814
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-10 20:06:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "app_service_default_hostname" {
-  value = azurerm_app_service.app.default_site_hostname
+  value = azurerm_linux_web_app.app.default_hostname
 }
 
 output "apim_gateway_url" {
   value = azurerm_api_management.apim.gateway_url
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "app_service_name" {
+  value = azurerm_linux_web_app.app.name
 }
