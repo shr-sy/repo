f5a3bca532ab8c14d5d5e65b804f58ea00d34416
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-12 17:05:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,3 +22,24 @@ variable "apim_name" {
   type        = string
   description = "API Management name"
 }
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure AD Tenant ID"
+}
+
+variable "oauth_client_id" {
+  type        = string
+  description = "Client ID from Azure AD App Registration"
+}
+
+variable "oauth_client_secret" {
+  type        = string
+  description = "Client Secret from Azure AD App Registration"
+  sensitive   = true
+}
+
+variable "api_audience" {
+  type        = string
+  description = "API App ID URI set in Azure AD"
+}
